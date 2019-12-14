32b365ec8453d213dc0835fda1e04fc8024e3309
breytblat <breytblat@gmail.com>
2019-12-13 22:11:41 -0500

    test

---
+++ test.py
@@ -0,0 +1,11 @@
+foo='your address'
+print(foo)
+ok=['danny','abby','sasha','nestor','ben']
+print(ok[0:1])
+print(ok[2:5])
+nah={'danny':14,'abby':15,'sasha':16,'nestor':16,'ben':59}
+print(nah['danny'])
+print(nah['abby'])
+print(nah['sasha'])
+print(nah['nestor'])
+print(nah['ben'])
